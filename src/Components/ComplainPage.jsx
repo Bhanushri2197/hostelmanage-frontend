8e5b4584b4f19468bcbec43c0545b920c264391f
Bhanushri2197 <bhanushri2197@gmail.com>
2024-11-13 20:57:10 +0530

    complain paGE

--- src/Components/ComplainPage.jsx
+++ src/Components/ComplainPage.jsx
@@ -1,46 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 import "../Styles/complainStyle.css";
-import { Link } from 'react-router-dom';
 
 function ComplainPage() {
   const [complains, setComplains] = useState([]);
-  
   const user = JSON.parse(localStorage.getItem('user'));
+  const navigate = useNavigate();
 
-  const getComplains = async () => {
-    try {
-      const complainsRes = await axios.get("https://hostelmanagement-backend-nbv7.onrender.com/complains/complain");
-      if (user && user.email) {
-        // Filter complaints to only show those created by the logged-in user
+  useEffect(() => {
+    // If the user is not logged in, redirect to the login page
+    if (!user) {
+      navigate('/login');
+      return;
+    }
+    
+    // Fetch complaints if user is logged in
+    const getComplains = async () => {
+      try {
+        const complainsRes = await axios.get("https://hostelmanagement-backend-nbv7.onrender.com/complains/complain");
+        // Show only complaints for the logged-in user
         const userComplains = complainsRes.data.filter(complain => complain.email === user.email);
         setComplains(userComplains);
-      } else {
-        // If no user is logged in, or no filtering is required
-        setComplains(complainsRes.data);
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    };
+
+    getComplains();
+  }, [user, navigate]);
 
   const handleDelete = (_id) => {
-    let confirmDelete = confirm("Are you sure you want to delete this complain?");
-    if (confirmDelete) {
+    if (confirm("Are you sure you want to delete this complain?")) {
       axios
         .delete(`https://hostelmanagement-backend-nbv7.onrender.com/complains/complain/${_id}`)
         .then(() => {
-          setComplains(complains.filter((complain) => complain._id !== _id));
+          setComplains(complains.filter(complain => complain._id !== _id));
         })
         .catch((error) => {
           console.error(error);
         });
     }
   };
-
-  useEffect(() => {
-    getComplains();
-  }, []);
 
   return (
     <div className='complainPage'>
@@ -50,23 +51,27 @@ function ComplainPage() {
           <Link to={'/add-complain'} className="btnPrimary addComplainBtn">Create Complain</Link>
         </div>
         <div className="complainsList d-flex flex-wrap justify-content-center">
-          {complains.map((complain, index) => (
-            <div key={index} className="complainItems">
-              <span className='status '>{complain.status}</span>
-              <div className="innerContent">
-                <div className="d-flex h-100 flex-column justify-content-between">
-                  <div className="d-flex flex-column">
-                    <h5 className="heading5">{complain.complainTitle} in room - {complain.roomNo}</h5>
-                    <div className="createdDate">Created at {complain.createdAt}</div>
-                    <div className="description">{complain.description}</div>
-                  </div>
-                  <div className="actionBlock p-3 d-flex justify-content-end">
-                    <button className="btn deleteBtn" onClick={() => handleDelete(complain._id)}>Delete</button>
+          {complains.length > 0 ? (
+            complains.map((complain, index) => (
+              <div key={index} className="complainItems">
+                <span className='status'>{complain.status}</span>
+                <div className="innerContent">
+                  <div className="d-flex h-100 flex-column justify-content-between">
+                    <div className="d-flex flex-column">
+                      <h5 className="heading5">{complain.complainTitle} in room - {complain.roomNo}</h5>
+                      <div className="createdDate">Created at {complain.createdAt}</div>
+                      <div className="description">{complain.description}</div>
+                    </div>
+                    <div className="actionBlock p-3 d-flex justify-content-end">
+                      <button className="btn deleteBtn" onClick={() => handleDelete(complain._id)}>Delete</button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            <p>No complaints found</p>
+          )}
         </div>
       </div>
     </div>
